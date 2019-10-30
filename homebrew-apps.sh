7c815d2a1ac3c07c01370452e1cba11711063241
Chris Charlton <chris.charlton@pantheon.io>
2019-10-29 21:07:05 -0700

    Updated software installed

--- homebrew-apps.sh
+++ homebrew-apps.sh
@@ -2,11 +2,19 @@
 
 # Install software via homebrew
 cask install \
+alfred \
 atom \
+bartender \
+bettersnaptool \
 bettertouchtool \
+caffeine\
 chrome \
 firefox \
+handbrake \
+imageoptim \
+postman \
 #sequelpro \
 slack \
+sourcetree \
 tableplus \
-virtuabox
+virtualbox
--- homebrew-formulas.sh
+++ homebrew-formulas.sh
@@ -3,22 +3,23 @@
 # Install software via homebrew
 echo "Installing common software"
 brew install \
+curl \
+openssl \
+openssl-devel \
 wget \
-curl
-#valgrind \
-#kcachegrind \
-#puppet
-
+zlib \
+zlib-devel
 
 # Web development
 echo "Installing web development software"
-
 brew install \
+kcachegrind \
 siege \
+valgrind \
 wraith
 
 # Misc. utilities
 echo "Installing misc. stuff"
-
 brew install \
+libdvdcss \
 youtube-dl
