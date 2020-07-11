5f8fe030edac6c262314e82503f7a463f86c8e15
Chris Charlton <chris.charlton@pantheon.io>
2020-07-10 19:41:51 -0700

    Some formula updates

--- homebrew-formulas.sh
+++ homebrew-formulas.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+# Run this early, just in case
+brew update
+
 # Install software via homebrew
 echo "Installing common software"
 brew install \
 curl \
 openssl \
-openssl-devel \
 wget \
-zlib \
-zlib-devel
+zlib
 
 # Web development
 echo "Installing web development software"
 brew install \
-kcachegrind \
 siege \
-valgrind \
 wraith
+
+# Additional web dev software(s)
+brew install qcachegrind --with-graphviz
+
+
 
 # Misc. utilities
 echo "Installing misc. stuff"
 brew install \
 libdvdcss \
+graphviz \
 youtube-dl
