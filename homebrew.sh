64fb1bfba9972a60645e6fe783667624735f2c0b
Chris Charlton <chris@chrischarlton.us>
2020-08-06 15:55:12 -0700

    Extended brew update commands

--- homebrew.sh
+++ homebrew.sh
@@ -6,6 +6,8 @@ if [[ $? != 0 ]] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
     brew update
+    brew upgrade
+    brew cask upgrade
 fi
 
 # Install software via homebrew
