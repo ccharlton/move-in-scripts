3b0e16fb40a145dd908b2ec3f23b14130be79369
Chris Charlton <chris@chrischarlton.us>
2020-08-06 18:16:01 -0700

    ZSH shell install tweaks

--- shell.sh
+++ shell.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 setup_shell_choice() {
-  if [ ! -d ".zsh" ]
+  if [ ! -d "$HOME/.zsh" ]
   then
       echo "Setting up 'Oh My Zsh' shell preference"
-      brew install zsh zsh-completions
+      brew install zsh
+
+      # Install Oh-My-ZSH
+      sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+      # Install ZSH additions
+      brew install zsh-completions zsh-syntax-highlighting
+
+      # Install ZSH themes
+      git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
       # Make it your default shell
       chsh -s $(which zsh)
   else
