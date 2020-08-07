92f233a122e19aefa4d4a8370a65f7e0500d82c0
Chris Charlton <chris@chrischarlton.us>
2020-08-06 21:55:56 -0700

    Updated web dev software lists
    Also cleaned up some sorted items

--- homebrew-formulas.sh
+++ homebrew-formulas.sh
@@ -14,16 +14,24 @@ zlib
 # Web development
 echo "Installing web development software"
 brew install \
-siege \
-wraith
+node \
+phantomjs \
+siege
+# Old
+#gem install wraith
+
+# VRT
+npm install -g backstopjs
 
 # Additional web dev software(s)
+brew install casperjs --devel
 brew install qcachegrind --with-graphviz
 
 # Misc. utilities
 echo "Installing misc. stuff"
 brew install \
-libdvdcss \
-graphviz \
 ffmpeg \
+graphviz \
+imagemagick \
+libdvdcss \
 youtube-dl 
