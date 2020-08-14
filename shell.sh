4c2ccceadfea38f7bee41d48f0f9789a0e1b229d
Chris Charlton <chris@chrischarlton.us>
2020-08-13 23:33:25 -0700

    ZSH default coloring

--- shell.sh
+++ shell.sh
@@ -20,6 +20,25 @@ setup_shell_choice() {
       echo 'export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters' >> ~/.zshenv
       echo 'source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
 
+      # Install color highlighting
+      cat << EOF >> ~/.zshrc
+      # Color highlighting
+      alias ls='ls --color=auto'
+      alias grep='grep --color=auto'
+      alias fgrep='fgrep --color=auto'
+      alias egrep='egrep --color=auto'
+      alias diff='diff --color=auto'
+      
+      # Color Man pages
+      export LESS_TERMCAP_mb=$'\e[1;32m'
+      export LESS_TERMCAP_md=$'\e[1;32m'
+      export LESS_TERMCAP_me=$'\e[0m'
+      export LESS_TERMCAP_se=$'\e[0m'
+      export LESS_TERMCAP_so=$'\e[01;33m'
+      export LESS_TERMCAP_ue=$'\e[0m'
+      export LESS_TERMCAP_us=$'\e[1;4;31m'
+      EOF
+
       # Make it your default shell
       chsh -s $(which zsh)
   else
