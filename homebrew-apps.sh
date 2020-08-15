2c5cdfb9db1fe8a6cededea678c7917ef896bee6
Chris Charlton <chris@chrischarlton.us>
2020-08-14 21:18:40 -0700

    Multiple apps added

--- homebrew-apps.sh
+++ homebrew-apps.sh
@@ -8,12 +8,20 @@ bartender \
 #bettersnaptool \
 bettertouchtool \
 caffeine \
+camera-live \
+camtwist \
 chrome \
+cocktail \
 # docker \
 firefox \
+gimp \
 handbrake \
 imageoptim \
+istat-menus \
 iterm2 \
+keyboard-maestro \
+kindle \
+obs \
 postman \
 sequel-pro \
 slack \
