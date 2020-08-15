2c2ef2826fcf1e9070b643ba072844416be26054
Chris Charlton <chris@chrischarlton.us>
2020-08-14 22:10:58 -0700

    Added a bunch of apps and two new categories
    - phpstorm
    - plex
    - sony-ps4-remote-play
    - steam
    - battle-net
    - vlc
    - blender
    - spotify
    - divvy
    - arduino
    - rectangle
    - meld
    - cyberduck
    - wireshark
    - appcleaner
    - Hardware set
    - Gaming set

--- homebrew-apps.sh
+++ homebrew-apps.sh
@@ -2,30 +2,73 @@
 
 # Install software via homebrew
 brew cask install \
+adobe-creative-cloud \
+aerial \
 alfred \
+another-redis-desktop-manager \
+appcleaner \
 atom \
 bartender \
 #bettersnaptool \
 bettertouchtool \
+blender \
 caffeine \
 camera-live \
 camtwist \
-chrome \
 cocktail \
+cyberduck \
+daisydisk \
+divvy \
 # docker \
 firefox \
+gas-mask \
 gimp \
+# gitkraken \
+grammarly \
+# google-chrome \
 handbrake \
 imageoptim \
 istat-menus \
 iterm2 \
 keyboard-maestro \
+# keycastr \
 kindle \
+# lens \
+meld \
+# metasploit \
+mysqlworkbench \
 obs \
+phpstorm \
+plex \
 postman \
+qlcolorcode \
+quicklook-json \
+# r \
+rectangle \
 sequel-pro \
+sip \
 slack \
+soulver \
 sourcetree \
+spotify \
 #tableplus \
 transmit \
-virtualbox
+virtualbox \
+virtualbox-extension-pack \
+vlc \
+webpquicklook \
+# wifi-explorer \
+wireshark
+
+# Gaming & Recreational
+brew cask install \
+battle-net \
+epic-games \
+sony-ps4-remote-play \
+steam
+
+# Hardware softwares
+brew cask install \
+arduino \
+elgato-stream-deck \
+raspberry-pi-imager
