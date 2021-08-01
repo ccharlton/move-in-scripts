03a94e0f30390163630315f03dc1aed75ac75e6f
Chris Charlton <chris.charlton@pantheon.io>
2021-08-01 13:37:55 -0700

    Xcode setup and license accept

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Start from here...
 start_here() {
   setup_work_home_folder
+  mac_setup_xcode_license
 }
 
 # Setup 'Work' folder
@@ -10,6 +11,12 @@ setup_work_home_folder() {
   source "work.sh"
   source "homebrew.sh"
   source "shell.sh"
+}
+
+# Setup Xcode and accept Xcode license to get other parts rolling
+mac_setup_xcode_license() {
+  xcode-select --install
+  xcodebuild -license
 }
 
 start_here
